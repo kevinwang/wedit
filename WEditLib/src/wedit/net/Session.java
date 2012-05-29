fd7c964b616b2fed8396e2a5a76d7476cbc3834a
Kevin Wang <born2run5000@yahoo.com>
2012-05-29 19:10:19 -0400

    Create Request and Session classes in WEditLib.

---
+++ WEditLib/src/wedit/net/Session.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wedit.net;
+
+/**
+ *
+ * @author Kevin Wang
+ */
+public class Session {
+    
+}
