4fdd7954c32829c25eddd561829048908c8e89ac
Kevin Wang <born2run5000@yahoo.com>
2012-05-29 18:54:15 -0400

    Add .gitignore and blank WEdit and WEditServer projects.

---
+++ WEdit/src/wedit/WEdit.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wedit;
+
+/**
+ *
+ * @author Kevin Wang
+ */
+public class WEdit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
---
+++ WEditServer/src/wedit/WEditServer.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wedit;
+
+/**
+ *
+ * @author Kevin Wang
+ */
+public class WEditServer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
