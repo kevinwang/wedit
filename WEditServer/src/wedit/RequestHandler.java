44a702b4df1be7b03a9ea454cac0ec336cd64f11
Kevin Wang <born2run5000@yahoo.com>
2012-05-31 00:38:08 -0400

    Begin writing RequestHandler to queue up and handle Requests.

---
+++ WEditServer/src/wedit/RequestHandler.java
@@ -0,0 +1,62 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wedit;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import wedit.net.BacktracedRequest;
+import wedit.net.Request;
+
+/**
+ *
+ * @author Kevin Wang
+ */
+public class RequestHandler {
+    private Queue<BacktracedRequest> requests;
+    private Thread handlerThread;
+    private final Object lock = new Object();
+    
+    public RequestHandler() {
+        requests = new LinkedList<BacktracedRequest>();
+        handlerThread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                while (true) {
+                    if (!requests.isEmpty()) {
+                        synchronized (lock) {
+                            BacktracedRequest r = requests.remove();
+                            switch (r.getRequestType()) {
+                                case Request.TYPE_INSERT:
+                                    break;
+                                case Request.TYPE_DELETE:
+                                    break;
+                                case Request.TYPE_CHAT:
+                                    break;
+                                case Request.TYPE_NICK:
+                                    String newNick = r.getData();
+                                    r.getOrigin().setNick(newNick);
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
+            
+        });
+    }
+    
+    public void startHandlerThread() {
+        handlerThread.start();
+    }
+    
+    public void addRequest(BacktracedRequest r) {
+        synchronized (lock) {
+            requests.add(r);
+        }
+    }
+}
